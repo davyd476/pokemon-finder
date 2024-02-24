0977795b3f03b12c58433304cbdba68a7cdc5809
davyd476 <davyd6095@gmail.com>
2024-02-24 19:59:46 +0100

    add link for pokemon info

--- js/index.js
+++ js/index.js
@@ -58,10 +58,7 @@ const addPokemonMarkup = ({ name, front_default }) => {
       </figure>
       <div class="article-body">
         <h2>${name}</h2>
-        <p>
-          Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
-        </p>
-        <a href="#" class="read-more">
+        <a target="_blank" href="https://bulbapedia.bulbagarden.net/wiki/${name}" class="read-more">
           Read more <span class="sr-only">about this is some title</span>
           <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
